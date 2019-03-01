c21d7a128991ad5a43c80a0163501ea8f50343c5
huhuta <smarthorse6@gmail.com>
2019-03-01 21:28:34 +0900

    Add react eslintrc

---
+++ eslintrc/react/eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'prettier/react',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    jest: true,
+    browser: true,
+  },
+  plugins: ['jest', 'react', 'import', 'promise', 'prettier'],
+  rules: {
+    'no-console': 'error',
+    'no-alert': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '_',
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'always',
+      },
+    ],
+  },
+}
--- eslintrc/react/install.sh
+++ eslintrc/react/install.sh
@@ -1,9 +1,10 @@
 npm install --save-dev \
+    jest \
     eslint \
     prettier \
     eslint-plugin-jest \
+    eslint-config-airbnb \
     eslint-plugin-import \
     eslint-plugin-promise \
     eslint-plugin-prettier \
-    eslint-config-prettier \
-    eslint-config-airbnb-base \
+    eslint-config-prettier
